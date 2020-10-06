c9efb922636463c8610fcd2f39a515f3f7305dce
Jakcuzi <cygan499@wp.pl>
2020-10-06 20:09:37 +0100

    Fixed some modal validation

--- src/views/Customers.vue
+++ src/views/Customers.vue
@@ -1,174 +1,148 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="customers"
-    sort-by="name"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" max-width="500px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="primary"
-            fab
-            dark
-            class="mb-2"
-            fixed
-            right
-            bottom
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    v-model="editedItem.name"
-                    label="Name"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    v-model="editedItem.phone"
-                    label="Phone"
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="4">
-                  <v-text-field
-                    v-model="editedItem.car"
-                    label="Car"
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-          </v-card-text>
-          <v-card-actions>
+    <v-data-table :headers="headers" :items="customers" sort-by="name" class="elevation-1">
+        <template v-slot:top>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon small fab color="primary" class="mr-2" @click="editItem(item)"
-        >mdi-pencil</v-icon
-      >
-      <v-icon small fab color="red" @click="deleteItem(item)"
-        >mdi-delete</v-icon
-      >
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
-  </v-data-table>
+            <v-dialog v-model="dialog" max-width="500px" persistent>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn color="primary" fab dark class="mb-2" fixed right bottom v-bind="attrs" v-on="on">
+                        <v-icon>mdi-plus</v-icon>
+                    </v-btn>
+                </template>
+                <v-form v-model="isValid">
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.name" label="Name" required :rules="[(v) => !!v || 'Name is required']"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.phone" label="Phone"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.cars" label="Cars"></v-text-field>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                            <v-btn color="blue darken-1" :disabled="!isValid" text @click="save">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-form>
+            </v-dialog>
+        </template>
+        <template v-slot:item.actions="{ item }">
+            <v-icon small fab color="primary" class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon small fab color="red" @click="deleteItem(item)">mdi-delete</v-icon>
+        </template>
+        <template v-slot:no-data>
+            <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+    </v-data-table>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    dialog: false,
-    headers: [
-      { text: "Name", align: "start", value: "name" },
-      { text: "Phone", value: "phone" },
-      { text: "Car", value: "car" },
-      { text: "Actions", value: "actions", sortable: false },
-    ],
-    customers: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      phone: "",
-      car: "",
-    },
-    defaultItem: {
-      name: "",
-      phone: "",
-      car: "",
-    },
-  }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New Customer" : "Edit Customer";
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    //get all customers endpoint call
-    initialize() {
-      this.customers = [
-        {
-          name: "John",
-          phone: "01234567891",
-          car: "EX123KK",
+    data: () => ({
+        isValid: true,
+        dialog: false,
+        headers: [
+            { text: "Name", align: "start", value: "name" },
+            { text: "Phone", value: "phone" },
+            { text: "Cars", value: "cars" },
+            { text: "Actions", value: "actions", sortable: false },
+        ],
+        customers: [],
+        editedIndex: -1,
+        editedItem: {
+            name: null,
+            phone: null,
+            cars: null,
         },
-        {
-          name: "John1",
-          phone: "01234567891",
-          car: "EX123KC",
+        defaultItem: {
+            name: null,
+            phone: null,
+            cars: null,
         },
-        {
-          name: "John2",
-          phone: "01234567891",
-          car: "EX123KB",
+    }),
+
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? "New Customer" : "Edit Customer";
         },
-        {
-          name: "John3",
-          phone: "01234567891",
-          car: "EX123KA",
+    },
+
+    watch: {
+        dialog(val) {
+            val || this.close();
         },
-      ];
     },
 
-    // call backend endpoints for these actions
-    editItem(item) {
-      this.editedIndex = this.customers.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+    created() {
+        this.initialize();
     },
 
-    deleteItem(item) {
-      const index = this.customers.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.customers.splice(index, 1);
-    },
+    methods: {
+        //get all customers endpoint call
+        initialize() {
+            this.customers = [
+                {
+                    name: "John",
+                    phone: "01234567891",
+                    cars: "EX123KK",
+                },
+                {
+                    name: "John1",
+                    phone: "01234567891",
+                    cars: "EX123KC, DEFGHI",
+                },
+                {
+                    name: "John2",
+                    phone: "01234567891",
+                    cars: "EX123KB",
+                },
+                {
+                    name: "John3",
+                    phone: "01234567891",
+                    cars: "EX123KA, ABCDEF",
+                },
+            ];
+        },
 
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
+        // call backend endpoints for these actions
+        editItem(item) {
+            this.editedIndex = this.customers.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialog = true;
+        },
 
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.customers[this.editedIndex], this.editedItem);
-      } else {
-        this.customers.push(this.editedItem);
-      }
-      this.close();
+        deleteItem(item) {
+            const index = this.customers.indexOf(item);
+            confirm("Are you sure you want to delete this item?") && this.customers.splice(index, 1);
+        },
+
+        close() {
+            this.dialog = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
+
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.customers[this.editedIndex], this.editedItem);
+            } else {
+                this.customers.push(this.editedItem);
+            }
+            this.close();
+        },
     },
-  },
 };
 </script>
--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,296 +1,219 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="jobs"
-    sort-by="date"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-form v-model="isValid">
-        <v-dialog
-          v-model="dialog"
-          hide-overlay
-          transition="dialog-bottom-transition"
-          fullscreen
-          persistent
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn
-              color="primary"
-              fab
-              dark
-              class="mb-2"
-              fixed
-              right
-              bottom
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>mdi-plus</v-icon>
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">{{ formTitle }}</span>
-            </v-card-title>
+    <v-data-table :headers="headers" :items="jobs" sort-by="date" class="elevation-1">
+        <template v-slot:top>
+            <v-form v-model="isValid" ref="form">
+                <v-dialog v-model="dialog" transition="dialog-bottom-transition" fullscreen persistent>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn color="primary" fab dark class="mb-2" fixed right bottom v-bind="attrs" v-on="on">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                    </template>
 
-            <!-- <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.reg"
-                      label="Reg"
-                      :rules="regRule"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.model"
-                      label="Model"
-                      :rules="modelRule"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.make"
-                      label="Make"
-                      :rules="makeRule"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.owner"
-                      label="Owner"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text> -->
-            <!--Full screen modal for adding new jobs-->
+                    <v-card>
+                        <v-card-title>
+                            <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
 
-            <v-form v-model="valid">
-              <v-container>
-                <v-row>
-                  <v-col cols="12" md="4">
-                    <v-text-field
-                      v-model="firstname"
-                      :rules="nameRules"
-                      :counter="10"
-                      label="First name"
-                      required
-                    ></v-text-field>
-                  </v-col>
+                        <v-form v-model="isValid">
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" md="4">
+                                        <v-text-field v-model="customer.firstname" :rules="nameRules" :counter="10" label="First name" required></v-text-field>
+                                    </v-col>
 
-                  <v-col cols="12" md="4">
-                    <v-text-field
-                      v-model="lastname"
-                      :rules="nameRules"
-                      :counter="10"
-                      label="Last name"
-                      required
-                    ></v-text-field>
-                  </v-col>
+                                    <v-col cols="12" md="4">
+                                        <v-text-field v-model="customer.lastname" :rules="nameRules" :counter="10" label="Last name" required></v-text-field>
+                                    </v-col>
 
-                  <v-col cols="12" md="4">
-                    <v-text-field
-                      v-model="email"
-                      :rules="emailRules"
-                      label="E-mail"
-                      required
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
+                                    <v-col cols="12" md="4">
+                                        <v-text-field v-model="customer.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-form>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+                            <v-btn color="blue darken-1" :disabled="!isValid" text @click="save">Save</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </v-form>
+        </template>
 
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn
-                color="blue darken-1"
-                :disabled="!isValid"
-                text
-                @click="save"
-                >Save</v-btn
-              >
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-form>
-    </template>
-
-    <template v-slot:item.actions="{ item }">
-      <v-icon small fab color="primary" class="mr-2" @click="editItem(item)"
-        >mdi-pencil</v-icon
-      >
-      <v-icon small fab color="red" @click="deleteItem(item)"
-        >mdi-delete</v-icon
-      >
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
-  </v-data-table>
+        <template v-slot:item.actions="{ item }">
+            <v-icon small fab color="primary" class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+            <v-icon small fab color="red" @click="deleteItem(item)">mdi-delete</v-icon>
+        </template>
+        <template v-slot:no-data>
+            <v-btn color="primary" @click="initialize">Reset</v-btn>
+        </template>
+    </v-data-table>
 </template>
 
 <script>
 export default {
-  //get all jobs endpoint call
-  data: () => ({
-    valid: false,
-    firstname: "",
-    lastname: "",
-    nameRules: [
-      (v) => !!v || "Name is required",
-      (v) => v.length <= 10 || "Name must be less than 10 characters",
-    ],
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+/.test(v) || "E-mail must be valid",
-    ],
-    isValid: true,
-    dialog: false,
-
-    regRule: [(v) => !!v || "Reg is required"],
-    modelRule: [(v) => !!v || "Model is required"],
-    makeRule: [(v) => !!v || "Make is required"],
-
-    headers: [
-      { text: "JobNo", value: "jobNo", align: "start" },
-      { text: "Date", value: "date" },
-      { text: "Car", value: "car" },
-      { text: "Description", value: "description" },
-      { text: "Parts", value: "parts" },
-      { text: "Actions", value: "actions", sortable: false },
-    ],
-    jobs: [],
-    editedIndex: -1,
-    editedItem: {
-      jobNo: null,
-      date: null,
-      car: null,
-      description: null,
-      parts: null,
-    },
-    defaultItem: {
-      jobNo: null,
-      date: null,
-      car: null,
-      description: null,
-      parts: null,
-    },
-  }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New" : "Edit";
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    //get all customers endpoint call
-    initialize() {
-      this.jobs = [
-        {
-          jobNo: "123",
-          date: "11/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
+    //get all jobs endpoint call
+    data: () => ({
+        isValid: true,
+        customers: [],
+        customer: {
+            firstname: null,
+            lastname: null,
+            email: null,
         },
-        {
-          jobNo: "123",
-          date: "11/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
+        nameRules: [(v) => !!v || "Name is required", (v) => v.length <= 10 || "Name must be less than 10 characters"],
+        emailRules: [(v) => !!v || "E-mail is required", (v) => /.+@.+/.test(v) || "E-mail must be valid"],
+
+        dialog: false,
+
+        regRule: [(v) => !!v || "Reg is required"],
+        modelRule: [(v) => !!v || "Model is required"],
+        makeRule: [(v) => !!v || "Make is required"],
+
+        headers: [
+            { text: "JobNo", value: "jobNo", align: "start" },
+            { text: "Date", value: "date" },
+            { text: "Car", value: "car" },
+            { text: "Description", value: "description" },
+            { text: "Parts", value: "parts" },
+            { text: "Actions", value: "actions", sortable: false },
+        ],
+        jobs: [],
+        editedIndex: -1,
+        editedItem: {
+            jobNo: null,
+            date: null,
+            car: null,
+            description: null,
+            parts: null,
         },
-        {
-          jobNo: "123",
-          date: "11/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
+        defaultItem: {
+            jobNo: null,
+            date: null,
+            car: null,
+            description: null,
+            parts: null,
         },
-        {
-          jobNo: "123",
-          date: "11/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
+    }),
+
+    computed: {
+        formTitle() {
+            return this.editedIndex === -1 ? "New Job" : "Edit Job";
         },
-        {
-          jobNo: "123",
-          date: "11/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
-        },
-        {
-          jobNo: "123",
-          date: "08/03/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
-        },
-        {
-          jobNo: "123",
-          date: "09/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
-        },
-        {
-          jobNo: "123",
-          date: "10/02/2020",
-          car: "Focus",
-          description: "some description",
-          parts: "List of parts used",
-        },
-      ];
     },
 
-    // call backend endpoints for these actions
-    editItem(item) {
-      this.editedIndex = this.jobs.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
+    watch: {
+        dialog(val) {
+            val || this.close();
+        },
     },
 
-    deleteItem(item) {
-      const index = this.jobs.indexOf(item);
-      confirm("Are you sure you want to delete this item?") &&
-        this.jobs.splice(index, 1);
+    created() {
+        this.initialize();
     },
 
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
+    methods: {
+        //get all customers endpoint call
+        initialize() {
+            this.jobs = [
+                {
+                    jobNo: "123",
+                    date: "11/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "11/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "11/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "11/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "11/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "08/03/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "09/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+                {
+                    jobNo: "123",
+                    date: "10/02/2020",
+                    car: "Focus",
+                    description: "some description",
+                    parts: "List of parts used",
+                },
+            ];
 
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.jobs[this.editedIndex], this.editedItem);
-      } else {
-        this.jobs.push(this.editedItem);
-      }
-      this.close();
+            this.customers = [
+                {
+                    firstname: "name",
+                    lastname: "surname",
+                    phone: "0123456789",
+                    cars: "",
+                },
+            ];
+        },
+
+        // call backend endpoints for these actions
+        editItem(item) {
+            this.editedIndex = this.jobs.indexOf(item);
+            this.editedItem = Object.assign({}, item);
+            this.dialog = true;
+        },
+
+        deleteItem(item) {
+            const index = this.jobs.indexOf(item);
+            confirm("Are you sure you want to delete this item?") && this.jobs.splice(index, 1);
+        },
+
+        close() {
+            this.dialog = false;
+            this.$nextTick(() => {
+                this.editedItem = Object.assign({}, this.defaultItem);
+                this.editedIndex = -1;
+            });
+        },
+
+        save() {
+            if (this.editedIndex > -1) {
+                Object.assign(this.jobs[this.editedIndex], this.editedItem);
+            } else {
+                this.jobs.push(this.editedItem);
+            }
+            this.close();
+        },
     },
-  },
 };
 </script>
